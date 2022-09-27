fd473d65a70f5d7849502725bf23dde14468ee60
Jamal Kaksouri <jamal.kaksouri@gmail.com>
2022-09-27 13:56:45 +0330

    init api-gateway

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
 proto:
-	protoc --go_out=. pkg/**/pb/*.proto
+	protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    pkg/**/pb/*.proto
 server:
 	go run cmd/main.go
+
+vet:
+	go vet cmd/main.go
+	shadow cmd/main.go
+.PHONY:vet
--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,61 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	opMap := map[string]opFuncType{
+		"+": add,
+		"-": sub,
+		"*": mul,
+		"/": div,
+	}
+
+	expressions := [][]string{
+		{"1", "+", "2"},
+		{"2", "-", "3"},
+		{"8", "*", "2"},
+		{"15", "/", "2"},
+		{"15", "%", "2"},
+		{"15"},
+		{"Two", "-", "One"},
+	}
+
+	for _, expression := range expressions {
+		if len(expression) < 3 {
+			fmt.Println("invalid expression", expression)
+			continue
+		}
+
+		p1, err := strconv.Atoi(expression[0])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		op := expression[1]
+		opFunc, ok := opMap[op]
+		if !ok {
+			fmt.Println("unsupported operator", op)
+			continue
+		}
+
+		p2, err := strconv.Atoi(expression[2])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		result := opFunc(p1, p2)
+		fmt.Println(result)
+	}
+}
+
+type opFuncType func(i, j int) int
+
+func add(i, j int) int { return i + j }
+func sub(i, j int) int { return i - j }
+func mul(i, j int) int { return i * j }
+func div(i, j int) int { return i / j }
